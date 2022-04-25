985e5e0250be20f1f1383a99f654bb51d96d66b5
smeea-2018 <smeeaarshad@gmail.com>
2022-04-25 22:34:09 +0100

    main functions

--- index.js
+++ index.js
@@ -63,3 +63,26 @@ const createRandomPassword = (passwordLength, passwordCriteria) => {
 
   return passwordGenerated.join("");
 };
+const generatePassword = () => {
+  // get the password length
+  const passwordLength = getPasswordLength();
+
+  // get the password criteria
+  const passwordCriteria = getPasswordCriteria();
+
+  // create random password
+  const password = createRandomPassword(passwordLength, passwordCriteria);
+
+  return password;
+};
+
+// Write password to the #password input
+const writePassword = () => {
+  const password = generatePassword();
+  const passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+};
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
